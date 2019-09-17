3db3b7de52a37871ed29a88feb33208523120010
dixiaochang <dixiaochang123@sina.com>
2019-09-18 00:18:12 +0800

    签到

--- src/routes/SignIn/index.js
+++ src/routes/SignIn/index.js
@@ -8,7 +8,8 @@ export default class SignIn extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            news: []
+            gold_coin:20,
+            seventh_days:7
 
         };
         // this.onRightClick = this.onRightClick.bind(this)
@@ -25,8 +26,18 @@ export default class SignIn extends Component {
                 rightContent={"签到规则"}
                 className='navbar'
             >签到有礼</NavBar>
+            {/* 签到规则跳转覆盖 */}
             <div className={style.onRightClick} onClick={()=>this.props.history.push('/signrule') }></div>
-
+            {/* 已连续签到 */}
+            <div className={style.continuity}>
+                <p>已连续签到</p>
+                <p><span>0</span><span>0</span><span>{this.state.seventh_days}</span>天</p>
+                <p>今日签到已获得{this.state.gold_coin}金币，连续签到{this.state.seventh_days}天金币翻倍</p>
+                <span className={style.radius}></span>
+                <span className={style.radius}></span>
+                <span className={style.radius}></span>
+            </div>
+            <p className={style.tody}>2019年9月</p>
 
         </div>)
     }
