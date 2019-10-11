77ff7ddc11610ac04f92b9c6cee5886501e8c5e7
dixiaochang <dixiaochang123@sina.com>
2019-10-11 22:10:47 +0800

    fetch封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -17,6 +17,35 @@ function checkStatus(response) {
   throw error;
 }
 
+const parseQuery = (obj) => {
+  let str = ''
+  for (let key in obj) {
+    const value = typeof obj[key] !== 'string' ? JSON.stringify(obj[key]) : obj[key]
+    str += '&' + key + '=' + value
+  }
+  return str.substr(1)
+}
+
+const request = (url, method = 'get', data) => {
+  const options = {
+    method: method,   // HTTP请求方法，默认为GET
+    headers: {        // HTTP的请求头，默认为{}
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include' // 是否携带cookie，默认为omit,不携带; same-origi,同源携带; include,同源跨域都携带
+  }
+  if (method === 'get') {
+    url += '?' + parseQuery(data)
+  } else {
+    options.body = JSON.stringify(data)
+  }
+  return fetch(url, options)
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(data => ({ data }))
+    .catch(err => ({ err }));
+}
+
 /**
  * Requests a URL, returning a promise.
  *
@@ -24,10 +53,19 @@ function checkStatus(response) {
  * @param  {object} [options] The options we want to pass to "fetch"
  * @return {object}           An object containing either "data" or "err"
  */
-export default function request(url, options) {
+/* export default function request(url, options) {
   return fetch(url, options)
     .then(checkStatus)
     .then(parseJSON)
     .then(data => ({ data }))
     .catch(err => ({ err }));
+} */
+
+export default {
+  get (url, data) {
+    return request(url, 'get', data)
+  },
+  post (url, data) {
+    return request(url, 'post', data)
+  }
 }
