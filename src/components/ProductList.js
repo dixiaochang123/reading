8bcbbe8bf870186cd54bc65118d4968db3461ace
dixiaochang123 <dixiaochang123@sina.com>
2019-10-23 13:30:55 +0800

    删除antd组件

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import { Table, Popconfirm, Button, Divider } from 'antd';
 const ProductList = ({ onDelete, products }) => {
-  const columns = [{
-    title: 'Name',
-    dataIndex: 'name',
-  }, {
-    title: 'Actions',
-    render: (text, record) => {
-      return (
-        <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
-          <Button>Delete</Button>
-        </Popconfirm>
-      );
-    },
-  }];
+  // const columns = [{
+  //   title: 'Name',
+  //   dataIndex: 'name',
+  // }, {
+  //   title: 'Actions',
+  //   render: (text, record) => {
+  //     return (
+  //       <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
+  //         <Button>Delete</Button>
+  //       </Popconfirm>
+  //     );
+  //   },
+  // }];
   return <div>111</div>
     {/* <Table
       dataSource={products}
