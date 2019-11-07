9cb8ffe8bd4ac936e8f8ec9b69cc8ae65e141bdb
dixiaochang <dixiaochang123@sina.com>
2019-11-07 23:43:24 +0800

    金币提现弹框

--- src/routes/GoldCoin/index.js
+++ src/routes/GoldCoin/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { NavBar, Icon } from 'antd-mobile';
+import { NavBar, Icon, Modal } from 'antd-mobile';
 import style from './index.less'
 import {  Link } from 'dva/router';
 import {  getCoinLog } from '../../services/example';
 import {goBack} from '../../utils/andohistoy'
 import {setCookie,getCookie} from '../../utils/cookie'
+
+const alert = Modal.alert;
 
 export default class GoldCoin extends Component {
     constructor(props) {
@@ -15,8 +17,11 @@ export default class GoldCoin extends Component {
             coinAll:2000,
             coinLogs:[],
             simpleUserResult:{
-
-            }
+                coin:0,
+                todayCoin:0,
+                totalCoin:0
+            },
+            dialogShow:false
 
         };
     }
@@ -25,12 +30,15 @@ export default class GoldCoin extends Component {
         setCookie('token',token,10000000000)//设置cookie   setCookie('sex','男', 10);
 
         getCoinLog().then(res=>{
-            let {code,data} = res.data;
+            let {data} = res.data;
             console.log(data)
-            this.setState({
-                coinLogs:data.coinLogs,
-                simpleUserResult:data.simpleUserResult
-            })
+            if(!!data) {
+
+                this.setState({
+                    coinLogs:data.coinLogs,
+                    simpleUserResult:data.simpleUserResult
+                })
+            }
         }).catch(error=>console.log(error))
     }
 
@@ -38,8 +46,15 @@ export default class GoldCoin extends Component {
         goBack()
     }
 
+    goldGoout = () => {
+        this.setState({
+            dialogShow:true
+        })
+
+    }
+
     render() {
-        let { simpleUserResult,coinLogs } = this.state;
+        let { simpleUserResult,coinLogs,dialogShow } = this.state;
         return (<div className='content'>
             <NavBar
                 mode="light"
@@ -52,15 +67,15 @@ export default class GoldCoin extends Component {
             <div className={style.contentsub}>
                 <div className={style.mygold}>
                     <p className={style.jbye}>金币余额</p>
-                    <h1>{simpleUserResult.coin ||0}</h1>
+                    <h1>{!!simpleUserResult && !!simpleUserResult.coin && simpleUserResult.coin ||0}</h1>
                     <div className={style.todyAndAll}>
                         <div>
                             <span>今日金币</span>
-                            <p>{simpleUserResult.todayCoin || 0}</p>
+                            <p>{!!simpleUserResult && !!simpleUserResult.todayCoin && simpleUserResult.todayCoin || 0}</p>
                         </div>
                         <div>
                             <span>累计获得</span>
-                            <p>{simpleUserResult.totalCoin || 0}</p>
+                            <p>{!!simpleUserResult && !!simpleUserResult.totalCoin && simpleUserResult.totalCoin || 0}</p>
                         </div>
                     </div>
                 </div>
@@ -69,7 +84,7 @@ export default class GoldCoin extends Component {
                 <h3>金币详情</h3>
                 <div className={style.icondetailsAll}>
                     {
-                        coinLogs.map(item=>{
+                        !!coinLogs && coinLogs.map(item=>{
 
                             return (<div className={style.coinLogs}>
                                 <div>
@@ -83,8 +98,18 @@ export default class GoldCoin extends Component {
 
                 </div>
                 <p className={style.tip}>金币详情只显示最近50条的记录</p>
-                <button className={style.btn}><Link to={'/cashout'}>金币提现</Link></button>
+                {/* <button className={style.btn}><Link to={'/cashout'}>金币提现</Link></button> */}
+                <button className={style.btn} onClick={this.goldGoout}>金币提现</button>
             </div>
+
+            <div className={dialogShow ? style.dialog : style.hide}>
+                <p>温馨提示</p>
+                <p>有空看书感谢您的关注，因应用市场 审核原因，提现功能暂缓开放。预计 于2020年1月15日前可开通提现功能， 敬请期待。</p>
+                <p>所有用户提现开通后可<span style={{color:"#FF6514"}}>双倍领取福利。</span></p>
+                <button className={style.btn1} onClick={()=>this.setState({dialogShow:false})}>我知道了</button>
+
+            </div>
+            <div className={dialogShow ? style.mode : style.hide}></div>
 
 
             
