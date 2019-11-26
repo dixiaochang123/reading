7f3d108f2910539808960b76bff5b260c71f254d
dixiaochang123 <dixiaochang123@sina.com>
2019-11-26 15:47:22 +0800

    login

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ function parseJSON(response) {
   if(token!="undefined" && token!=null && token!='null' && token!=undefined && token!="") {
     response.headers.token = token
   } else {
-    // login()
+    login()
   }
   // response.headers.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjEiLCJleHAiOjE2MDE2MDQ2MDN9.ifSBbRKyHjZK5Dv5QUJP5tkzZTgy4Z48JrYRYwewc9k';
   
